60edfb06ec4ac5ef75702da49ff2c00a580f175f
Luis Jorge (MAC) <gevolutionxalapa@gmail.com>
2025-12-26 14:53:42 -0600

    La notacion Pathvariable

---
+++ src/main/java/com/gevolution/curso/controllers/GreetingRestController.java
@@ -0,0 +1,13 @@
+package com.gevolution.curso.controllers;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+public class GreetingRestController {
+
+    @GetMapping("/saludo/{name}")
+    public String greeting(@PathVariable String name) {
+        return "Hola "+name;
+    }
+}
