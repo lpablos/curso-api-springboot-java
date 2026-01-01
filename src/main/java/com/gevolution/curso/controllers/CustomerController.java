7fa24db7c3a193656e1f80c2b9c1c3d7bdd3f30b
Luis Jorge (MAC) <gevolutionxalapa@gmail.com>
2026-01-01 17:39:03 -0600

    Creacion de un api rest full

---
+++ src/main/java/com/gevolution/curso/controllers/CustomerController.java
@@ -0,0 +1,30 @@
+package com.gevolution.curso.controllers;
+import com.gevolution.curso.domain.Customer;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+@RestController
+public class CustomerController {
+    
+    // Lista para ontener los clientes
+
+    private List<Customer> customers = new ArrayList<>(
+        Arrays.asList(
+            new Customer(1, "Juan", "juan123", "juan@mail.com", "123"),
+            new Customer(2, "Ana", "ana123", "ana@mail.com", "123"),
+            new Customer(3, "Luis", "luis123", "luis@mail.com", "123"),
+            new Customer(4, "Maria", "maria123", "maria@mail.com", "123")
+        )
+    );
+    
+    @GetMapping("/clientes")  
+    public List<Customer> getCustomers(){
+        return customers;
+    }
+
+}
