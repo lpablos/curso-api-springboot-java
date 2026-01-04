e0445655007a3f662329d29a3e998436a0d4ced1
Luis Jorge (MAC) <gevolutionxalapa@gmail.com>
2026-01-03 19:54:35 -0600

    GetMaping con parametro

--- src/main/java/com/gevolution/curso/controllers/CustomerController.java
+++ src/main/java/com/gevolution/curso/controllers/CustomerController.java
@@ -2,6 +2,7 @@ package com.gevolution.curso.controllers;
 import com.gevolution.curso.domain.Customer;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,10 +22,20 @@ public class CustomerController {
             new Customer(4, "Maria", "maria123", "maria@mail.com", "123")
         )
     );
-    
+
     @GetMapping("/clientes")  
     public List<Customer> getCustomers(){
         return customers;
+    }
+
+    @GetMapping("/cliente/{username}")
+    public Customer getCliente(@PathVariable String username){
+        for(Customer c : customers){
+            if(c.getUsername().equalsIgnoreCase(username)){
+                return c;
+            }
+        }
+        return null;
     }
 
 }
